3973dbda913d94b5899afb346df6e987c24b0585
Jan Jaap <mail@janjaap.de>
2021-08-15 01:19:27 +0200

    feat(docker): actually build the admin ui

--- admin-ui/src/api/index.ts
+++ admin-ui/src/api/index.ts
@@ -1,5 +1,4 @@
 import {FerdigClient} from '@ferdig/client-js';
-import {getEnvVar} from '@/utils/env';
 
 let client: FerdigClient | null = null;
 
@@ -9,7 +8,7 @@ export type GenericDocumentType = Record<string, GenericDocumentTypeTypes>;
 export function getFerdigClient(): FerdigClient {
     if (!client) {
         client = new FerdigClient({
-            host: getEnvVar('VUE_APP_FERDIG_HOST', 'string'),
+            host: location.origin,
         });
     }
 
