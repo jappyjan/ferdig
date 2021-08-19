baf6be89c507e941158dcc5f60ec9f07520ede7b
Jan Jaap <mail@janjaap.de>
2021-08-19 21:52:12 +0200

    feat(file-upload): options type

--- src/controllers/Applications/Collections/Documents/UploadAnyFileMiddleware.ts
+++ src/controllers/Applications/Collections/Documents/UploadAnyFileMiddleware.ts
@@ -1,8 +1,8 @@
-import Multer from 'multer';
+import Multer, {Options} from 'multer';
 import {multerConfig} from '../../../../config/sub-configs/multerConfig';
 import {IMiddleware, Middleware, Next, Req, Res} from '@tsed/common';
 
-const MulterAnyFileMiddleware = (Multer(multerConfig)).any();
+const MulterAnyFileMiddleware = (Multer(multerConfig as unknown as Options)).any();
 
 @Middleware()
 export class UploadAnyFileMiddleware implements IMiddleware {
